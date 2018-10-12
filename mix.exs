751bfb53f99e7923c950d857da0ee5b833427ae4
Dan Rees-Jones <dan.reesjones@carwow.co.uk>
2018-10-12 11:12:40 +0100

    update httpoison

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule CatAlert.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:httpoison, "~> 1.1"},
+      {:httpoison, "~> 1.3"},
       {:poison, "~> 3.1"},
       {:bamboo, "~> 0.8"},
       {:extwitter, "~> 0.8"}
